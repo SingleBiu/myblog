05d9152b7256d5f6300651180ec5373367ed1563
SingleBiu <854390236@qq.com>
2022-11-24 13:58:13 +0800

    first commit

---
+++ routers/commentsRouter__________go_src_myblog_controllers.go
@@ -0,0 +1,19 @@
+package routers
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context/param"
+)
+
+func init() {
+
+    beego.GlobalControllerRouter["myblog/controllers:MainController"] = append(beego.GlobalControllerRouter["myblog/controllers:MainController"],
+        beego.ControllerComments{
+            Method: "Get",
+            Router: `/`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+}
